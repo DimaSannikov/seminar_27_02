cc9e4aea9c9a47c134256f79b45002f71ceb51e9
DimaSannikov <dimasannikov@gmail.com>
2023-02-28 00:43:20 +0500

    added EX_04

---
+++ EX_04/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.Write("Введите число: ");
+int digit = int.Parse(Console.ReadLine());
+
+int num7 = digit % 7;
+int num23 = digit % 23;
+
+if(num7 == 0 & num23 == 0)
+{
+    Console.WriteLine($"Число {digit} кратно 7 и 23");
+}
+else
+{
+    Console.WriteLine($"Число {digit} не кратно ни 7, ни 23");
+}
