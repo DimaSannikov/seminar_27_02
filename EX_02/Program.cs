21c53ed982434f899dc7b5ce2cecc93129b5954d
DimaSannikov <dimasannikov@gmail.com>
2023-02-28 00:43:01 +0500

    added EX_02

---
+++ EX_02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное трехзначное 
+// число и удаляет вторую цифру этого числа.
+
+int number = new Random().Next(100, 1000);
+
+int firstDigit = number / 100;
+int secondDigit = number % 10;
+
+Console.WriteLine(number);
+// Console.WriteLine(firstDigit);
+// Console.WriteLine(secondDigit);
+
+int newDigit = firstDigit * 10 + secondDigit;
+
+Console.WriteLine(newDigit);
