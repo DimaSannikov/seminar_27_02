7835be17df136ae89ef2c581aef0c269db1a59dc
DimaSannikov <dimasannikov@gmail.com>
2023-02-28 00:44:04 +0500

    added HW_02

---
+++ HW_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного
+// числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int digit = int.Parse(Console.ReadLine());
+int num = 0;
+
+if(digit > 99)
+{
+    while(digit > 99)
+    {
+        num = digit % 10;
+        digit /= 10;
+    }
+    Console.WriteLine(num);
+}
+else
+{
+    Console.WriteLine("Введенное число состоит менее чем из трех цифр");
+}
