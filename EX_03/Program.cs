f725c0fcdecec9271f0000807c1239a42f7e08be
DimaSannikov <dimasannikov@gmail.com>
2023-02-28 00:43:11 +0500

    added EX_03

---
+++ EX_03/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая будет принимать на вход два числа
+// и выводить, является ли второе число кратным первому. 
+// Если число 1 не кратно числу 2, то программа выводит остаток
+// от деления.
+
+Console.Write("Введите число 1: ");
+int digit1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число 2: ");
+int digit2 = int.Parse(Console.ReadLine());
+
+int num = digit1 % digit2;
+
+if(num == 0)
+{
+    Console.WriteLine($"Число кратно {digit2}");
+}
+else
+{
+    Console.WriteLine($"Число некратно {digit2}");
+    Console.WriteLine($"Остаток от деления {num}");
+}
